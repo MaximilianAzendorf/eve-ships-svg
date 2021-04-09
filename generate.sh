1bab3d4a4ffc816b2fad65a4a424348d27b24a8d
Maximilian Azendorf <max.azendorf@outlook.com>
2021-04-10 01:32:41 +0200

    Adjust CSS and generation script!

--- generate.sh
+++ generate.sh
@@ -8,4 +8,9 @@ cp -r eve-ships/output/full docs/svg
 rm -rf eve-ships/output
 pushd docs
 	zip -r all.zip svg/
+	pushd svg
+		echo "var svgs = [" > ../shiplist.js
+		ls -c1 | sort | sed -e "s/^/\"/" | sed -e "s/\$/\",/" >> ../shiplist.js
+		echo "]" >> ../shiplist.js
+	popd
 popd
